699743340767d1095e984dff5a91b2073c541ab7
syKim12 <fromsykim@gmail.com>
2022-08-03 16:49:25 +0900

    change codes to get json

--- dags/s3_upload_s3hook.py
+++ dags/s3_upload_s3hook.py
@@ -6,18 +6,15 @@ from airflow.hooks.S3_hook import S3Hook
 AWS_CONN_ID = 'aws_default'
 
 def upload_to_s3() -> None:
-    import urllib, json
+    import urllib, json, requests
     #get json
-    url = 'http://openapi.seoul.go.kr:8088/7a5555736979656f3435534668576f/json/CardSubwayStatsNew/1/1000/20220301'
-    # url을 불러오고 이것을 인코딩을 utf-8로 전환하여 결과
-    response = urllib.request.urlopen(url) 
-    json_str = response.read().decode("utf-8")
-    index = json_str.find("USE_DT") # json 파싱 위해 index
-    upload_str = "{\"SUBWAY_STATS\":["+json_str[index-2:-1]
+    result=requests.get(f'http://openapi.seoul.go.kr:8088/7a5555736979656f3435534668576f/json/CardSubwayStatsNew/1/1000/20220301')
+    data=result.json()
+    json_str = json.dumps(data['CardSubwayStatsNew']['row'], ensure_ascii=False)
   
     #upload to s3
     hook = S3Hook(aws_conn_id=AWS_CONN_ID)
-    hook.load_string(upload_str, key='20220301_str_chg.json', bucket_name='subway-json-bkt-sykim')
+    hook.load_string(json_str, key='20220301_str_dump.json', bucket_name='subway-json-bkt-sykim')
 
 
 with DAG(
