b4a6e50516eb1b053894d4ea707530120491b6c3
syKim12 <fromsykim@gmail.com>
2022-08-04 11:18:42 +0900

    fix errors of csv file

--- dags/s3_upload.py
+++ dags/s3_upload.py
@@ -1,18 +1,16 @@
 from datetime import datetime
 from airflow.models import DAG
-import json, boto3, requests, urllib
+import json, boto3, urllib
 
-url = 'http://openapi.seoul.go.kr:8088/7a5555736979656f3435534668576f/json/CardSubwayStatsNew/1/1000/20220301'
-
-
-# url을 불러오고 이것을 인코딩을 utf-8로 전환하여 결과를 받자.
-response = urllib.request.urlopen(url) 
-json_str = response.read().decode("utf-8")
-
-# 받은 데이터가 문자열이라서 이를 json으로 변환한다.
-json_object = json.loads(json_str)
-
-s3 = boto3.client('s3')
+def upload_json() -> None:
+    url = 'http://openapi.seoul.go.kr:8088/7a5555736979656f3435534668576f/json/CardSubwayStatsNew/1/1000/20220301'
+    # url을 불러오고 이것을 인코딩을 utf-8로 전환하여 결과를 받자.
+    response = urllib.request.urlopen(url) 
+    json_str = response.read().decode("utf-8")
+    # 받은 데이터가 문자열이라서 이를 json으로 변환한다.
+    json_object = json.loads(json_str)
+    s3 = boto3.client('s3')
+    s3.meta.client.upload_file('/tmp/hello.txt', 'mybucket', 'hello.txt')
 
 with DAG(
     dag_id='s3_upload',
