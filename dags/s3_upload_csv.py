8be54f87f397f103aa2ecacd6715fb63309925ca
syKim12 <fromsykim@gmail.com>
2022-08-12 14:48:34 +0900

    to get 202207 subway data

--- dags/s3_upload_csv.py
+++ dags/s3_upload_csv.py
@@ -7,28 +7,31 @@ AWS_CONN_ID = 'aws_default'
 
 def upload_to_s3() -> None:
     import urllib, json, requests, csv
-    #get json
-    result=requests.get(f'http://openapi.seoul.go.kr:8088/7a5555736979656f3435534668576f/json/CardSubwayStatsNew/1/1000/20220301')
-    data=result.json()
-    json_object = data['CardSubwayStatsNew']['row']
-    
-    #convert to csv
-    data_file = open('data_file.csv', 'w', encoding='utf-8-sig', newline='')
+    #open csv file
+    date_count = 0
+    data_file = open('data_file_202207.csv', 'w', encoding='utf-8-sig', newline='')
     csv_writer = csv.writer(data_file)
+    #get json
+    secrets = json.loads(open('secrets.json').read())
+    subway_api_key = secrets["SUBWAY_API_KEY"]
+    for date in range(20220701, 20220732):
+        result=requests.get(f'http://openapi.seoul.go.kr:8088/{subway_api_key}/json/CardSubwayStatsNew/1/1000/{date}')
+        data=result.json()
+        json_object = data['CardSubwayStatsNew']['row']
 
-    count = 0
-    for data in json_object:
-        if count == 0:
-            header = data.keys()
-            csv_writer.writerow(header)
-            count += 1
-        csv_writer.writerow(data.values())
-
+        count = 0
+        for data in json_object:
+            if count == 0 and date_count==0:
+                header = data.keys()
+                csv_writer.writerow(header)
+                count += 1
+            csv_writer.writerow(data.values())
+        date_count += 1
     data_file.close()
 
     #upload to s3
     hook = S3Hook(aws_conn_id=AWS_CONN_ID)
-    hook.load_file(filename='data_file.csv', key='20220301_subway_utf8.csv', bucket_name='subway-csv-bkt-sykim')
+    hook.load_file(filename='data_file_202207.csv', key='202207_subway_utf8.csv', bucket_name='subway-csv-bkt-sykim')
 
 
 with DAG(
